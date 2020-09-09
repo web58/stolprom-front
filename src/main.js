57a15bedab52f1d1bd34be4f0f35bf67b33d1435
DANILIKYS <ooo.kerm@yandex.ru>
2020-09-09 14:12:42 +0300

    шапка - фул

--- src/main.js
+++ src/main.js
@@ -6,6 +6,18 @@ jQuery(function($) {
     $('body').toggleClass('header_opened');
   });
 
+  if (window.matchMedia("(min-width: 1200px)").matches) {
+
+    $(window).on("scroll", function () {
+      scroll = $(this).scrollTop();
+      if (scroll > 110) {
+        $('body').addClass('header-scrolled');
+      } else {
+        $('body').removeClass('header-scrolled');
+      }
+    });
+  }
+
 
 
 });
