1230903a3e7d2089dded8b358a59be10c7aa3543
DANILIKYS <ooo.kerm@yandex.ru>
2020-09-16 15:24:53 +0300

    + TIMER

--- src/main.js
+++ src/main.js
@@ -198,6 +198,54 @@ jQuery(function($) {
   //ABOUT PAGE SLIDERS FINISH
 
 
+  //  TIMER START
+  function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector(".days");
+    var hoursSpan = clock.querySelector(".hours");
+    var minutesSpan = clock.querySelector(".minutes");
+    // var secondsSpan = clock.querySelector(".seconds");
+
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+
+      if (t.total <= 0) {
+        $('#discount').css('display', 'none');
+        clearInterval(timeinterval);
+        return true;
+      }
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+      minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+      // secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 10000);
+  }
+
+  var deadlineEndless = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000); // for endless timer
+  var deadline = $('.discount').attr('data-deadline');
+  initializeClock('discount', deadline);
+  //  TIMER FINISH
+
+
 });
 
 
